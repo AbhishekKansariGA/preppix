443db54e5b3834d9e499dc9cc50da05d4a45a903
Hacks & Mods <priyadubey700024@gmail.com>
2025-09-21 18:51:46 +0000

    Font ubuntu select

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,9 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
-        poppins: ['Poppins', 'sans-serif'],
+        body: ['Ubuntu', 'sans-serif'],
+        headline: ['Ubuntu', 'sans-serif'],
+        poppins: ['Ubuntu', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
