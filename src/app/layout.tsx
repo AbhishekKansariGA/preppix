703e307cd1cc5737a6fc0a156ee084be2cec3295
Hacks & Mods <priyadubey700024@gmail.com>
2025-09-22 16:49:21 +0000

    At top it hides my welcome text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         <AuthProvider>
             <div className="relative flex min-h-screen flex-col">
               <Header/>
-              <main className="flex-1 container mx-auto px-4 py-8">
+              <main className="flex-1 container mx-auto px-4 py-8 pt-24">
                 {children}
               </main>
             </div>
